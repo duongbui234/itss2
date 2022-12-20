de4601f144589a1d31efca68bfeefe24cfcb9da7
Dam Dung <dung.dam@dac-datatech.vn>
2022-12-20 12:07:09 +0700

    feat: test history

--- frontend/src/components/PlayGames/Result.js
+++ frontend/src/components/PlayGames/Result.js
@@ -31,12 +31,75 @@ function convertQuesToCoin(nRight = 0, nWrong = 0, currentCoin = 0) {
   return newCoin;
 }
 
-function CorrectWordResult({ nRight, nWrong, nRightConsecutive, onReplay }) {
+function CorrectWordResult({
+  gameHistory,
+  nRight,
+  nWrong,
+  nRightConsecutive,
+  onReplay,
+}) {
   const classes = cwResultStyle();
   const history = useHistory();
   const dispatch = useDispatch();
   const { isAuth, coin } = useSelector((state) => state.userInfo);
-
+  // gameHistory = [
+  //   {
+  //     word: 'indicate',
+  //     mean: 'thể hiện, trình bày',
+  //     phonetic: 'ˈɪndɪkeɪt',
+  //     wrongList: [
+  //       { word: 'show', phonetic: 'ʃəʊ' },
+  //       { word: 'indicate', phonetic: 'ˈɪndɪkeɪt' },
+  //       { word: 'conduct', phonetic: 'kənˈdʌkt' },
+  //     ],
+  //     userAnswer: 'indicate',
+  //   },
+  //   {
+  //     word: 'conduct',
+  //     mean: 'triển khai, thực hiện',
+  //     phonetic: 'kənˈdʌkt',
+  //     wrongList: [
+  //       { word: 'show', phonetic: 'ʃəʊ' },
+  //       { word: 'indicate', phonetic: 'ˈɪndɪkeɪt' },
+  //       { word: 'conduct', phonetic: 'kənˈdʌkt' },
+  //     ],
+  //     userAnswer: 'assure',
+  //   },
+  //   {
+  //     word: 'assure',
+  //     mean: 'đảm bảo, cam đoan',
+  //     phonetic: 'əˈʃʊə(r)',
+  //     wrongList: [
+  //       { word: 'show', phonetic: 'ʃəʊ' },
+  //       { word: 'indicate', phonetic: 'ˈɪndɪkeɪt' },
+  //       { word: 'conduct', phonetic: 'kənˈdʌkt' },
+  //     ],
+  //     userAnswer: 'show',
+  //   },
+  //   {
+  //     word: 'show',
+  //     mean: 'thể hiện, chỉ ra',
+  //     phonetic: 'ʃəʊ',
+  //     wrongList: [
+  //       { word: 'show', phonetic: 'ʃəʊ' },
+  //       { word: 'indicate', phonetic: 'ˈɪndɪkeɪt' },
+  //       { word: 'conduct', phonetic: 'kənˈdʌkt' },
+  //     ],
+  //     userAnswer: 'assure',
+  //   },
+  //   {
+  //     word: 'assign',
+  //     mean: 'chỉ định',
+  //     phonetic: 'əˈsaɪn',
+  //     wrongList: [
+  //       { word: 'show', phonetic: 'ʃəʊ' },
+  //       { word: 'indicate', phonetic: 'ˈɪndɪkeɪt' },
+  //       { word: 'conduct', phonetic: 'kənˈdʌkt' },
+  //     ],
+  //     userAnswer: 'assure',
+  //   },
+  // ];
+  // console.log(gameHistory);
   // play win audio
   useEffect(() => {
     onPlayAudio(winAudioSrc);
@@ -74,41 +137,62 @@ function CorrectWordResult({ nRight, nWrong, nRightConsecutive, onReplay }) {
   };
 
   return (
-    <div className={`${classes.root} flex-center-col`}>
-      <img className={classes.img} src={cupIcon} alt="Cup Photo" />
-
-      <div className={`${classes.result} flex-center--ver`}>
-        <b>{nRight}</b>&nbsp;Đúng
-        <RightIcon className={`${classes.icon} right`} />
-        &nbsp;-&nbsp;
-        <b>{nRightConsecutive}</b>&nbsp;Đúng liên tiếp
-        <RightIcon className={`${classes.icon} right`} />
-        &nbsp;-&nbsp;
-        <b>{nWrong}</b>&nbsp;Sai
-        <WrongIcon className={`${classes.icon} wrong`} />
-      </div>
-
-      {isAuth && (
-        <div className={`${classes.result} flex-center--ver mt-4`}>
-          <CoinIcon
-            className={classes.icon}
-            style={{ color: '#C3AD1A', marginLeft: 0 }}
-          />
-          Số coin hiện tại:&nbsp;<b>{coin}</b>
+    <div>
+      <div className={`${classes.root} flex-center-col`}>
+        <img className={classes.img} src={cupIcon} alt="Cup Photo" />
+        <div className={`${classes.result} flex-center--ver`}>
+          <b>{nRight}</b>&nbsp;Đúng
+          <RightIcon className={`${classes.icon} right`} />
+          &nbsp;-&nbsp;
+          <b>{nRightConsecutive}</b>&nbsp;Đúng liên tiếp
+          <RightIcon className={`${classes.icon} right`} />
+          &nbsp;-&nbsp;
+          <b>{nWrong}</b>&nbsp;Sai
+          <WrongIcon className={`${classes.icon} wrong`} />
         </div>
-      )}
 
-      <div className="mt-10">
-        <Button
-          className="_btn _btn-outlined-stand mr-5"
-          variant="outlined"
-          onClick={onGoBack}>
-          Quay về
-        </Button>
-        <Button className="_btn _btn-primary" onClick={onReplay}>
-          Chơi lại
-        </Button>
+        {isAuth && (
+          <div className={`${classes.result} flex-center--ver mt-4`}>
+            <CoinIcon
+              className={classes.icon}
+              style={{ color: '#C3AD1A', marginLeft: 0 }}
+            />
+            Số coin hiện tại:&nbsp;<b>{coin}</b>
+          </div>
+        )}
+
+        <div className="mt-10">
+          <Button
+            className="_btn _btn-outlined-stand mr-5"
+            variant="outlined"
+            onClick={onGoBack}>
+            Quay về
+          </Button>
+          <Button className="_btn _btn-primary" onClick={onReplay}>
+            Chơi lại
+          </Button>
+        </div>
       </div>
+      &nbsp;&nbsp;
+      <div>Lịch sử bài thi</div>
+      {gameHistory.map((answer, index) => (
+        <div>
+          <div>
+            Câu hỏi: <span>{answer.mean}</span>
+          </div>
+          <div>
+            Đáp án:
+            <span>{answer.word}</span>
+            {answer.wrongList.map((wrongAnswer, index) => (
+              <div>
+                <span>{wrongAnswer.word}</span>
+              </div>
+            ))}
+            <span>Bạn đã chọn: {answer.userAnswer}</span>
+          </div>
+          &nbsp;&nbsp;
+        </div>
+      ))}
     </div>
   );
 }
